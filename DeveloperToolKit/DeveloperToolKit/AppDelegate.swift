d34bb330e67a0843ce749018333a02344545796f
xuwenxue <xuwenxue@kuaishou.com>
2018-09-29 17:21:17 +0800

    init

---
+++ DeveloperToolKit/DeveloperToolKit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DeveloperToolKit
+//
+//  Created by william on 2018/9/29.
+//  Copyright © 2018 william. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DeveloperToolKit/DeveloperToolKit/Document.swift
@@ -0,0 +1,29 @@
+//
+//  Document.swift
+//  DeveloperToolKit
+//
+//  Created by william on 2018/9/29.
+//  Copyright © 2018 william. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
+        self.addWindowController(windowController)
+    }
+
+}
