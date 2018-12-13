c28717a09a8e22f243e424de9e2178dad946373b
xuwenxue <xuwenxue@kuaishou.com>
2018-12-13 10:30:27 +0800

    change view controller

---
+++ DeveloperToolKit/DeveloperToolKit/TabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewController.swift
+//  DeveloperToolKit
+//
+//  Created by william on 2018/12/13.
+//  Copyright © 2018 william. All rights reserved.
+//
+
+import Cocoa
+
+class TabViewController: NSTabViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
